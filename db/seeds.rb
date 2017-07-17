075b925254791a855b3b43252a2b0f80927d65d4
Chadwick Flores <chadwickflores@gmail.com>
2017-07-17 18:11:10 -0500

    added seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Record.create!(title: "Record A", date: Date.today, amount: 500)
+Record.create!(title: "Record B", date: Date.today, amount: -200)
+Record.create!(title: "Record C", date: Date.today, amount: 100.25)
+Record.create!(title: "Record D", date: Date.today, amount: -150)
+Record.create!(title: "Record E", date: Date.today, amount: 800)
+
+puts "5 records have been created"
