2163b19067913de60a1ceb3cbb7d443cf51beb4c
Chadwick Flores <chadwickflores@gmail.com>
2017-07-17 16:00:15 -0500

    added editing records inline

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -9,7 +9,16 @@ class RecordsController < ApplicationController
     if @record.save
       render json: @record
     else
-      render json: @record.errors, status: :unproccessable_entity
+      render json: @record.errors, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    @record = Record.find(params[:id])
+    if @record.update(record_params)
+      render json: @record
+    else
+      render json: @record.errors, status: :unprocessable_entity
     end
   end
 
